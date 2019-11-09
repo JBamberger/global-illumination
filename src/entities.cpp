50d35a0430080d806af6b8b687694f5ac07ed45c
Jannik Bamberger <dev.jbamberger@gmail.com>
2019-11-09 23:18:57 +0100

    add tests for normal and intersection of sphere

--- src/entities.cpp
+++ src/entities.cpp
@@ -2,33 +2,44 @@
 #include "entities.h"
 
 bool ImplicitSphere::intersect(const Ray& ray, glm::dvec3& intersect, glm::dvec3& normal) const {
-    // O = ray.origin, D = ray.dir, this->radius = R, this->pos = C
+    // O = ray.origin
+    // D = ray.dir
+    // R = this->radius
+    // C = this->pos
+    //
     // (P - C)^2 - R^2 = 0 = (O + Dt - C)^2 - R^2
     // O^2 + 2ODt - 2CDt - 2CO + (Dt)^2 + C^2 - R^2 = 0
     // ( D^2 )t^2 + ( 2DO - 2DC )t + ( O^2 - 2DO + C^2 - R^2  ) = 0
     // ( D^2 )t^2 + ( 2D(O - C) )t + ( (O - C)^2 - R^2  ) = 0
+    //
     // x1,x2 = (b -+ sqrt( b^2 - 4ac ))/ 2a
     // q = b^2 - 4ac
     // q = (2D(O-C))^2 - 4 * D^2 ((O-C)^2 - R^2)
 
-    glm::dvec3 L = ray.origin - pos;
-    double a = glm::dot(ray.dir, ray.dir);
-    double b = 2. * glm::dot(ray.dir, L);
-    double c = glm::dot(L, L) - (radius * radius);
+    const auto L = ray.origin - pos;
+    const auto a = glm::dot(ray.dir, ray.dir);
+    const auto b = 2. * glm::dot(ray.dir, L);
+    const auto c = glm::dot(L, L) - (radius * radius);
 
     // q < 0 -> no solutions
     // q = 0 -> one solution
     // q > 0 -> two solutions
-    double q = b * b - 4 * a * c;
+    const auto q = b * b - 4 * a * c;
     double x1, x2; // possible solutions
     if (q < 0) {
         return false;
-    } else if (q == 0) {
+    }
+    if (q == 0) {
         x1 = x2 = -0.5 * b / a;
     } else {
-        double root = glm::sqrt(q);
-        x1 = 0.5 * (b - root) / a;
-        x2 = 0.5 * (b + root) / a;
+        const auto root = glm::sqrt(q);
+        x1 = -0.5 * (b - root) / a;
+        x2 = -0.5 * (b + root) / a;
+
+        // We want the point closer to the origin of the ray. Because the direction vector points in
+        // positive direction, a smaller value results in a closer point.
+        if (x1 > x2)
+            std::swap(x1, x2);
     }
 
     // TODO: select correct solution
--- test/entities-test.cpp
+++ test/entities-test.cpp
@@ -4,12 +4,14 @@
 #include <ostream>
 
 struct ray_spec {
-    bool success;
-    glm::dvec3 direction;
+    const bool success;
+    const glm::dvec3 direction;          // ray direction
+    const glm::dvec3 expected_intersect; // intersection point
+    const glm::dvec3 expected_normal;    // normal at the intersection point
 
-    friend std::ostream& operator<<(std::ostream& os, const ray_spec& obj) {
-        return os << "success: " << obj.success << " direction: " << glm::to_string(obj.direction);
-    }
+    ray_spec(const bool s, const glm::dvec3 d, const glm::dvec3 i, const glm::dvec3 n)
+        : success(s), direction(glm::normalize(d)), expected_intersect(i),
+          expected_normal(glm::normalize(n)) {}
 };
 
 struct TriangleIntersectionTest : public testing::TestWithParam<ray_spec> {
@@ -28,23 +30,21 @@ TEST_P(TriangleIntersectionTest, testSuccess) {
     auto params = GetParam();
     Ray ray{{10, 0, 0}, params.direction};
     bool success = triangle.intersect(ray, intersect, normal);
-    EXPECT_EQ(params.success, success);
+    EXPECT_EQ(success, params.success);
 }
 
-INSTANTIATE_TEST_CASE_P(Default,
-                        TriangleIntersectionTest,
-                        testing::Values( // inside triangle
-                            ray_spec{1, glm::dvec3{-10, 0.1, 0.1}},
-                            // exact border hits
-                            ray_spec{0, glm::dvec3{-1, 0, 0}}, // bottom left corner
-                            // outside tests
-                            ray_spec{0, glm::dvec3{-10, -0.1, 0.1}},  // left
-                            ray_spec{0, glm::dvec3{-10, 0.1, -0.1}},  // bottom
-                            ray_spec{0, glm::dvec3{-10, 0.7, 0.7}},   // right
-                            ray_spec{0, glm::dvec3{-10, -0.1, -0.1}}, // bottom left
-                            ray_spec{0, glm::dvec3{-10, 1.5, -0.1}},  // bottom right
-                            ray_spec{0, glm::dvec3{-10, -0.1, 1.5}}   // top right
-                            ));
+// clang-format off
+INSTANTIATE_TEST_CASE_P(Default, TriangleIntersectionTest, testing::Values(
+    ray_spec{1, glm::dvec3{-10,  0.1,  0.1}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // inside triangle
+    ray_spec{0, glm::dvec3{-1,   0,    0  }, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // exact hit test bottom left corner
+    ray_spec{0, glm::dvec3{-10, -0.1,  0.1}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // outside test left
+    ray_spec{0, glm::dvec3{-10,  0.1, -0.1}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // outside test bottom
+    ray_spec{0, glm::dvec3{-10,  0.7,  0.7}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // outside test right
+    ray_spec{0, glm::dvec3{-10, -0.1, -0.1}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // outside test bottom left
+    ray_spec{0, glm::dvec3{-10,  1.5, -0.1}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}}, // outside test bottom right
+    ray_spec{0, glm::dvec3{-10, -0.1,  1.5}, glm::dvec3{0,0,0}, glm::dvec3{0,0,0}})  // outside test top right
+);
+// clang-format on
 
 struct ImplicitSphereIntersectionTest : public testing::TestWithParam<ray_spec> {
     ImplicitSphere sphere;
@@ -59,33 +59,60 @@ TEST_P(ImplicitSphereIntersectionTest, testSuccess) {
     Ray ray{{10, 0, 0}, params.direction};
     bool success = sphere.intersect(ray, intersect, normal);
 
-    EXPECT_EQ(params.success, success);
+    EXPECT_EQ(success, params.success);
 }
 
-INSTANTIATE_TEST_CASE_P(Default,
-                        ImplicitSphereIntersectionTest,
-                        testing::Values( // center
-                            ray_spec{1, glm::dvec3{-1, 0, 0}},
-                            // right
-                            ray_spec{0, glm::dvec3{-1, 1, 0}},
-                            ray_spec{1, glm::dvec3{-0.04, 0.00401, 0}},
-                            ray_spec{1, glm::dvec3{-0.04, 0.00402, 0}},
-                            ray_spec{0, glm::dvec3{-0.04, 0.00403, 0}},
-                            // left
-                            ray_spec{0, glm::dvec3{-1, -1, 0}},
-                            ray_spec{1, glm::dvec3{-0.04, -0.00401, 0}},
-                            ray_spec{1, glm::dvec3{-0.04, -0.00402, 0}},
-                            ray_spec{0, glm::dvec3{-0.04, -0.00403, 0}},
-                            // top
-                            ray_spec{0, glm::dvec3{-1, 0, 1}},
-                            ray_spec{1, glm::dvec3{-0.04, 0, 0.00401}},
-                            ray_spec{1, glm::dvec3{-0.04, 0, 0.00402}},
-                            ray_spec{0, glm::dvec3{-0.04, 0, 0.00403}},
-                            // bottom
-                            ray_spec{0, glm::dvec3{-1, 0, -1}},
-                            ray_spec{1, glm::dvec3{-0.04, 0, -0.00401}},
-                            ray_spec{1, glm::dvec3{-0.04, 0, -0.00402}},
-                            ray_spec{0, glm::dvec3{-0.04, 0, -0.00403}}));
+TEST_P(ImplicitSphereIntersectionTest, testIntersect) {
+    auto params = GetParam();
+    Ray ray{{10, 0, 0}, params.direction};
+    sphere.intersect(ray, intersect, normal);
+
+    const double eps = 1e-5;
+    if (params.success) {
+        EXPECT_NEAR(intersect.x, params.expected_intersect.x, eps);
+        EXPECT_NEAR(intersect.y, params.expected_intersect.y, eps);
+        EXPECT_NEAR(intersect.z, params.expected_intersect.z, eps);
+    }
+}
+
+TEST_P(ImplicitSphereIntersectionTest, testNormal) {
+    auto params = GetParam();
+    Ray ray{{10, 0, 0}, params.direction};
+    sphere.intersect(ray, intersect, normal);
+
+    const double eps = 1e-5;
+    if (params.success) {
+        EXPECT_NEAR(normal.x, params.expected_normal.x, eps);
+        EXPECT_NEAR(normal.y, params.expected_normal.y, eps);
+        EXPECT_NEAR(normal.z, params.expected_normal.z, eps);
+    }
+}
+
+// clang-format off
+INSTANTIATE_TEST_CASE_P(Default, ImplicitSphereIntersectionTest, testing::Values( 
+    ray_spec{1, glm::dvec3{-1,             0,             0           }, glm::dvec3{1,             0,             0           }, glm::dvec3{1,            0,             0            }}, // center
+                                                                                                                                                                                      
+    ray_spec{0, glm::dvec3{-1,             1,             0           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }}, // right  far outside
+    ray_spec{0, glm::dvec3{-1,            -1,             0           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }}, // left   far outside
+    ray_spec{0, glm::dvec3{-1,             0,             1           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }}, // top    far outside
+    ray_spec{0, glm::dvec3{-1,             0,            -1           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }}, // bottom far outside
+
+    ray_spec{1, glm::dvec3{-9.8955634474,  0.9945315513,  0           }, glm::dvec3{0.1044365526,  0.9945315513,  0           }, glm::dvec3{0.0086595509,  0.0861623095,  0           }}, // right  close inside
+    ray_spec{1, glm::dvec3{-9.8955634474, -0.9945315513,  0           }, glm::dvec3{0.1044365526, -0.9945315513,  0           }, glm::dvec3{0.0086595509, -0.0861623095,  0           }}, // left   close inside
+    ray_spec{1, glm::dvec3{-9.8955634474,  0,             0.9945315513}, glm::dvec3{0.1044365526,  0,             0.9945315513}, glm::dvec3{0.0086595509,  0,             0.0861623095}}, // top    close inside
+    ray_spec{1, glm::dvec3{-9.8955634474,  0,            -0.9945315513}, glm::dvec3{0.1044365526,  0,            -0.9945315513}, glm::dvec3{0.0086595509,  0,            -0.0861623095}}, // bottom close inside
+
+    ray_spec{1, glm::dvec3{-9.9,           0.9949874371,  0           }, glm::dvec3{0.1,           0.9949874371,  0           }, glm::dvec3{0.0400670164,  0.3986617801,  0           }}, // right  exact
+    ray_spec{1, glm::dvec3{-9.9,          -0.9949874371,  0           }, glm::dvec3{0.1,          -0.9949874371,  0           }, glm::dvec3{0.0400670164, -0.3986617801,  0           }}, // left   exact
+    ray_spec{1, glm::dvec3{-9.9,           0,             0.9949874371}, glm::dvec3{0.1,           0,             0.9949874371}, glm::dvec3{0.0400670164,  0,             0.3986617801}}, // top    exact
+    ray_spec{1, glm::dvec3{-9.9,           0,            -0.9949874371}, glm::dvec3{0.1,           0,            -0.9949874371}, glm::dvec3{0.0400670164,  0,            -0.3986617801}}, // bottom exact
+                                          
+    ray_spec{0, glm::dvec3{-0.04,          0.00403,       0           }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }}, // right  close outside
+    ray_spec{0, glm::dvec3{-0.04,         -0.00403,       0           }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }}, // left   close outside
+    ray_spec{0, glm::dvec3{-0.04,          0,             0.00403     }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }}, // top    close outside
+    ray_spec{0, glm::dvec3{-0.04,          0,            -0.00403     }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }}) // bottom close outside
+);
+// clang-format on
 
 TEST(ImplicitSphere, center) {
     ImplicitSphere s1;
