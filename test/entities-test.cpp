4c0249ebd127ff1b023748d170b3ef9446c4f18c
Jannik Bamberger <dev.jbamberger@gmail.com>
2019-11-12 20:49:48 +0100

    fix test case for triangle intersection

--- test/entities-test.cpp
+++ test/entities-test.cpp
@@ -10,7 +10,7 @@ struct ray_spec {
     const glm::dvec3 direction;          // ray direction
     const glm::dvec3 expected_intersect; // intersection point
     const glm::dvec3 expected_normal;    // normal at the intersection point
-    const std::string description;       // testcase name
+    const std::string description;       // test case name
 
     ray_spec(const bool s,
              const glm::dvec3 d,
@@ -81,15 +81,15 @@ TEST_P(TriangleIntersectionTest, testNormal)
 }
 
 // clang-format off
-INSTANTIATE_TEST_SUITE_P(Default, TriangleIntersectionTest, testing::Values(
-    ray_spec{1, glm::dvec3{-10,  0.1,  0.1}, glm::dvec3{0,0.1,0.1}, glm::dvec3{1,0,0}, "inside triangle"                  },
-    ray_spec{0, glm::dvec3{-1,   0,    0  }, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "exact hit test bottom left corner"},
-    ray_spec{0, glm::dvec3{-10, -0.1,  0.1}, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "outside test left"                },
-    ray_spec{0, glm::dvec3{-10,  0.1, -0.1}, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "outside test bottom"              },
-    ray_spec{0, glm::dvec3{-10,  0.7,  0.7}, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "outside test right"               },
-    ray_spec{0, glm::dvec3{-10, -0.1, -0.1}, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "outside test bottom left"         },
-    ray_spec{0, glm::dvec3{-10,  1.5, -0.1}, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "outside test bottom right"        },
-    ray_spec{0, glm::dvec3{-10, -0.1,  1.5}, glm::dvec3{0,0,0},     glm::dvec3{0,0,0}, "outside test top right"           })
+INSTANTIATE_TEST_SUITE_P(InsideOutside, TriangleIntersectionTest, testing::Values(
+    ray_spec{true,  {-10,  0.1,  0.1}, {0, 0.1, 0.1}, {1, 0, 0}, "inside triangle"                  },
+    ray_spec{true,  {-1,   0,    0  }, {0, 0,   0  }, {1, 0, 0}, "exact hit test bottom left corner"},
+    ray_spec{false, {-10, -0.1,  0.1}, {0, 0,   0  }, {0, 0, 0}, "outside test left"                },
+    ray_spec{false, {-10,  0.1, -0.1}, {0, 0,   0  }, {0, 0, 0}, "outside test bottom"              },
+    ray_spec{false, {-10,  0.7,  0.7}, {0, 0,   0  }, {0, 0, 0}, "outside test right"               },
+    ray_spec{false, {-10, -0.1, -0.1}, {0, 0,   0  }, {0, 0, 0}, "outside test bottom left"         },
+    ray_spec{false, {-10,  1.5, -0.1}, {0, 0,   0  }, {0, 0, 0}, "outside test bottom right"        },
+    ray_spec{false, {-10, -0.1,  1.5}, {0, 0,   0  }, {0, 0, 0}, "outside test top right"           })
 );
 // clang-format on
 
@@ -137,34 +137,39 @@ TEST_P(ImplicitSphereIntersectionTest, testNormal)
 }
 
 // clang-format off
-INSTANTIATE_TEST_SUITE_P(Default, ImplicitSphereIntersectionTest, testing::Values( 
-    ray_spec{1, glm::dvec3{-1,             0,             0           }, glm::dvec3{1,             0,             0           }, glm::dvec3{1,            0,             0            }, "center"			   },
-    ray_spec{0, glm::dvec3{-1,             1,             0           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "right  far outside"  },
-    ray_spec{0, glm::dvec3{-1,            -1,             0           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "left   far outside"  },
-    ray_spec{0, glm::dvec3{-1,             0,             1           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "top    far outside"  },
-    ray_spec{0, glm::dvec3{-1,             0,            -1           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "bottom far outside"  },
-    ray_spec{1, glm::dvec3{-9.8955634474,  0.9945315513,  0           }, glm::dvec3{0.1044365526,  0.9945315513,  0           }, glm::dvec3{0.1021811887,  0.9730540950,  0           }, "right  close inside" },
-    ray_spec{1, glm::dvec3{-9.8955634474, -0.9945315513,  0           }, glm::dvec3{0.1044365526, -0.9945315513,  0           }, glm::dvec3{0.1021811887, -0.9730540950,  0           }, "left   close inside" },
-    ray_spec{1, glm::dvec3{-9.8955634474,  0,             0.9945315513}, glm::dvec3{0.1044365526,  0,             0.9945315513}, glm::dvec3{0.1021811887,  0,             0.9730540950}, "top    close inside" },
-    ray_spec{1, glm::dvec3{-9.8955634474,  0,            -0.9945315513}, glm::dvec3{0.1044365526,  0,            -0.9945315513}, glm::dvec3{0.1021811887,  0,            -0.9730540950}, "bottom close inside" },
-    ray_spec{1, glm::dvec3{-9.9,           0.9949874371,  0           }, glm::dvec3{0.1,           0.9949874371,  0           }, glm::dvec3{0.0400670164,  0.3986617801,  0           }, "right  exact"		   },
-    ray_spec{1, glm::dvec3{-9.9,          -0.9949874371,  0           }, glm::dvec3{0.1,          -0.9949874371,  0           }, glm::dvec3{0.0400670164, -0.3986617801,  0           }, "left   exact"		   },
-    ray_spec{1, glm::dvec3{-9.9,           0,             0.9949874371}, glm::dvec3{0.1,           0,             0.9949874371}, glm::dvec3{0.0400670164,  0,             0.3986617801}, "top    exact"		   },
-    ray_spec{1, glm::dvec3{-9.9,           0,            -0.9949874371}, glm::dvec3{0.1,           0,            -0.9949874371}, glm::dvec3{0.0400670164,  0,            -0.3986617801}, "bottom exact"		   },
-    ray_spec{0, glm::dvec3{-0.04,          0.00403,       0           }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "right  close outside"},
-    ray_spec{0, glm::dvec3{-0.04,         -0.00403,       0           }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "left   close outside"},
-    ray_spec{0, glm::dvec3{-0.04,          0,             0.00403     }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "top    close outside"},
-    ray_spec{0, glm::dvec3{-0.04,          0,            -0.00403     }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "bottom close outside"})
-);
+INSTANTIATE_TEST_SUITE_P(InsideOutside, ImplicitSphereIntersectionTest, testing::Values(
+    ray_spec{true,  glm::dvec3{-1,             0,             0           }, glm::dvec3{1,             0,             0           }, glm::dvec3{1,            0,             0            }, "center"			   },
+
+    ray_spec{false, glm::dvec3{-1,             1,             0           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "right  far outside"  },
+    ray_spec{false, glm::dvec3{-1,            -1,             0           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "left   far outside"  },
+    ray_spec{false, glm::dvec3{-1,             0,             1           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "top    far outside"  },
+    ray_spec{false, glm::dvec3{-1,             0,            -1           }, glm::dvec3{0,             0,             0           }, glm::dvec3{0,            0,             0            }, "bottom far outside"  }
+));
+INSTANTIATE_TEST_SUITE_P(CloseToBorder, ImplicitSphereIntersectionTest, testing::Values(
+    ray_spec{true,  glm::dvec3{-9.8955634474,  0.9945315513,  0           }, glm::dvec3{0.1044365526,  0.9945315513,  0           }, glm::dvec3{0.1021811887,  0.9730540950,  0           }, "right  close inside" },
+    ray_spec{true,  glm::dvec3{-9.8955634474, -0.9945315513,  0           }, glm::dvec3{0.1044365526, -0.9945315513,  0           }, glm::dvec3{0.1021811887, -0.9730540950,  0           }, "left   close inside" },
+    ray_spec{true,  glm::dvec3{-9.8955634474,  0,             0.9945315513}, glm::dvec3{0.1044365526,  0,             0.9945315513}, glm::dvec3{0.1021811887,  0,             0.9730540950}, "top    close inside" },
+    ray_spec{true,  glm::dvec3{-9.8955634474,  0,            -0.9945315513}, glm::dvec3{0.1044365526,  0,            -0.9945315513}, glm::dvec3{0.1021811887,  0,            -0.9730540950}, "bottom close inside" },
+
+    ray_spec{true,  glm::dvec3{-9.9,           0.9949874371,  0           }, glm::dvec3{0.1,           0.9949874371,  0           }, glm::dvec3{0.0400670164,  0.3986617801,  0           }, "right  exact"		   },
+    ray_spec{true,  glm::dvec3{-9.9,          -0.9949874371,  0           }, glm::dvec3{0.1,          -0.9949874371,  0           }, glm::dvec3{0.0400670164, -0.3986617801,  0           }, "left   exact"		   },
+    ray_spec{true,  glm::dvec3{-9.9,           0,             0.9949874371}, glm::dvec3{0.1,           0,             0.9949874371}, glm::dvec3{0.0400670164,  0,             0.3986617801}, "top    exact"		   },
+    ray_spec{true,  glm::dvec3{-9.9,           0,            -0.9949874371}, glm::dvec3{0.1,           0,            -0.9949874371}, glm::dvec3{0.0400670164,  0,            -0.3986617801}, "bottom exact"		   },
+
+    ray_spec{false, glm::dvec3{-0.04,          0.00403,       0           }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "right  close outside"},
+    ray_spec{false, glm::dvec3{-0.04,         -0.00403,       0           }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "left   close outside"},
+    ray_spec{false, glm::dvec3{-0.04,          0,             0.00403     }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "top    close outside"},
+    ray_spec{false, glm::dvec3{-0.04,          0,            -0.00403     }, glm::dvec3{0,             0,            0            }, glm::dvec3{0,             0,             0           }, "bottom close outside"}
+));
 // clang-format on
 
-TEST(ImplicitSphere, center)
-{
-    implicit_sphere s1;
-    s1.radius = 1;
-
-    glm::dvec3 a, b;
-    Ray r1{{10, 0, 0}, {-1, 0, 0}};
-
-    ASSERT_TRUE(s1.intersect(r1, a, b));
-}
+// TEST(ImplicitSphere, center)
+//{
+//    implicit_sphere s1;
+//    s1.radius = 1;
+//
+//    glm::dvec3 a, b;
+//    Ray r1{{10, 0, 0}, {-1, 0, 0}};
+//
+//    ASSERT_TRUE(s1.intersect(r1, a, b));
+//}
