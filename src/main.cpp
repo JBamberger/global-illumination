7a555fb64941f8eccc1f352872f69ba56965f59b
Jannik Bamberger <dev.jbamberger@gmail.com>
2019-11-25 21:57:55 +0100

    add scene creation functions in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,49 +20,56 @@ constexpr glm::dvec3 magenta(1, 0, 1);
 
 // dark blue glm::dvec3 { 0.3, 0.3, 1 }
 
-int main(int argc, char** argv)
+std::vector<std::unique_ptr<entity>> create_sphere_scene()
 {
-    QApplication app(argc, argv);
+    std::vector<std::unique_ptr<entity>> scene;
+    auto s1 = std::make_unique<implicit_sphere>(glm::dvec3{0, 2, 0}, 1.0);
+    auto s2 = std::make_unique<implicit_sphere>(glm::dvec3{0, -2, 0}, 1.0);
+    auto s3 = std::make_unique<implicit_sphere>(glm::dvec3{0, 0, 2}, 1.0);
+    auto s4 = std::make_unique<implicit_sphere>(glm::dvec3{0, 0, -2}, 1.0);
+    s1->material = std::make_shared<simple_material>(blue);
+    s2->material = std::make_shared<simple_material>(green);
+    s3->material = std::make_shared<simple_material>(red);
+    s4->material = std::make_shared<simple_material>(yellow);
+    scene.push_back(std::move(s1));
+    scene.push_back(std::move(s2));
+    scene.push_back(std::move(s3));
+    scene.push_back(std::move(s4));
 
-    Camera camera(glm::dvec3{10, 0, 0});
-    glm::dvec3 light{10, 10, 15};
+    return scene;
+}
 
-    RayTracer raytracer(camera, light);
-
-    auto mat_red = std::make_shared<simple_material>(red);
-    auto mat_green = std::make_shared<simple_material>(green);
-    auto mat_blue = std::make_shared<simple_material>(blue);
-
-    // Set up scene
-    octree scene({-20, -20, -20}, {20, 20, 20});
-
+std::vector<std::unique_ptr<entity>> create_complex_scene()
+{
     using namespace entities;
+
+    std::vector<std::unique_ptr<entity>> scene;
 
     // explicit sphere
     auto esphere = make_sphere(glm::dvec3{0, -2, 2}, 0.5, 1);
     esphere->material = std::make_shared<simple_material>(blue);
     esphere->material->ambient = 0.3;
-    scene.push_back(esphere.get());
+    scene.push_back(std::move(esphere));
 
     // explicit cube
     auto cube = make_cube(glm::dvec3{0, -2, 0}, 0.5);
     cube->material = std::make_shared<simple_material>(red);
-    scene.push_back(cube.get());
+    scene.push_back(std::move(cube));
 
     // Defines x,y and z axis indicators
     const auto tip = glm::dvec3{0, 0, 0};
     const auto id_len = 2;
     auto x_axis = make_cone(tip + glm::dvec3{id_len, 0, 0}, tip, 0.1, 10);
     x_axis->material = std::make_shared<simple_material>(red);
-    scene.push_back(x_axis.get());
+    scene.push_back(std::move(x_axis));
 
     auto y_axis = make_cone(tip + glm::dvec3{0, id_len, 0}, tip, 0.1, 10);
     y_axis->material = std::make_shared<simple_material>(green);
-    scene.push_back(y_axis.get());
+    scene.push_back(std::move(y_axis));
 
     auto z_axis = make_cone(tip + glm::dvec3{0, 0, id_len}, tip, 0.1, 10);
     z_axis->material = std::make_shared<simple_material>(blue);
-    scene.push_back(z_axis.get());
+    scene.push_back(std::move(z_axis));
 
     auto sphere = std::make_unique<implicit_sphere>(glm::dvec3{0, 0, 0}, 1.0);
     sphere->material = std::make_shared<checkerboard_material>();
@@ -81,23 +88,23 @@ int main(int argc, char** argv)
     // sphere->material->reflective = 0;
     // sphere->material->refractive = 1;
     // sphere->material->refractive_index = 1;
-    scene.push_back(sphere.get());
+    scene.push_back(std::move(sphere));
 
     // triangle upper right corner
     auto t3 = std::make_unique<triangle>(glm::dvec3{1, 2.5, 0.5}, glm::dvec3{1, 2.5, 2.5},
                                          glm::dvec3{1, 0.5, 2.5});
     t3->material = std::make_shared<simple_material>(magenta);
-    scene.push_back(t3.get());
+    scene.push_back(std::move(t3));
 
     auto t1 = std::make_unique<triangle>(glm::dvec3{-1, 1.5, -0.5}, glm::dvec3{-1, 1.5, 1.5},
                                          glm::dvec3{-1, -0.5, 1.5});
     t1->material = std::make_shared<simple_material>(cyan);
-    scene.push_back(t1.get());
+    scene.push_back(std::move(t1));
 
     auto t2 =
         std::make_unique<triangle>(glm::dvec3{0, 2, 0}, glm::dvec3{0, 2, 2}, glm::dvec3{0, 0, 2});
     t2->material = std::make_shared<simple_material>(yellow);
-    scene.push_back(t2.get());
+    scene.push_back(std::move(t2));
 
     // floor
     auto quad = make_quad({10, 10, -1}, {-10, 10, -1}, {-10, -10, -1}, {10, -10, -1});
@@ -107,7 +114,30 @@ int main(int argc, char** argv)
     quad->material->ambient = 0.0;
     quad->material->diffuse = 0.3;
     quad->material->specular = 0.7;
-    scene.push_back(quad.get());
+    scene.push_back(std::move(quad));
+
+    return scene;
+}
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    Camera camera(glm::dvec3{10, 0, 0});
+    glm::dvec3 light{10, 10, 15};
+
+    RayTracer raytracer(camera, light);
+
+    auto mat_red = std::make_shared<simple_material>(red);
+    auto mat_green = std::make_shared<simple_material>(green);
+    auto mat_blue = std::make_shared<simple_material>(blue);
+
+    // Set up scene
+    octree scene({-20, -20, -20}, {20, 20, 20});
+
+    // auto elems = create_sphere_scene();
+    auto elems = create_complex_scene();
+    for (const auto& entity : elems) scene.push_back(entity.get());
 
     raytracer.set_scene(&scene);
 
