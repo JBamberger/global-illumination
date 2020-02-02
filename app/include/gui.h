cd26c3904a27cdb0176c5a3bd4ba51cf51cb726b
Jannik Bamberger <dev.jbamberger@gmail.com>
2020-02-02 12:12:35 +0100

    Create menu bar and move timer

--- app/include/gui.h
+++ app/include/gui.h
@@ -4,8 +4,10 @@
 #include <QImage>
 #include <QLabel>
 #include <QMainWindow>
+#include <QMenuBar>
 #include <QPushButton>
 #include <QToolBar>
+#include <QStatusBar>
 #include <utility>
 
 #include "RayTracer.h"
@@ -13,43 +15,60 @@
 
 class Gui : public QMainWindow {
     QLabel* duration_text_;
-    QPushButton* save_button_;
+//    QPushButton* save_button_;
     Viewer* viewer_;
+    QMenu* fileMenu_;
+    QAction* saveAction_;
 
   public:
     Gui() = delete;
 
     Gui(const int width, const int height, RayTracer raytracer, QWindow* = nullptr)
     {
-        auto toolbar = new QToolBar(this);
-        toolbar->setMovable(false);
 
-        save_button_ = new QPushButton("Save as ...", this);
-        toolbar->addWidget(save_button_);
-
-        auto spacer = new QWidget();
-        spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-        toolbar->addWidget(spacer);
+//        auto toolbar = new QToolBar(this);
+//        toolbar->setMovable(false);
+//
+//        save_button_ = new QPushButton("Save as ...", this);
+//        toolbar->addWidget(save_button_);
+//
+//        auto spacer = new QWidget();
+//        spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+//        toolbar->addWidget(spacer);
 
         duration_text_ = new QLabel(this);
-        toolbar->addWidget(duration_text_);
-        this->addToolBar(toolbar);
+//        toolbar->addWidget(duration_text_);
+//        this->addToolBar(toolbar);
 
         viewer_ = new Viewer(std::move(raytracer), duration_text_, this);
         this->setCentralWidget(viewer_);
 
+        statusBar()->insertPermanentWidget(0, duration_text_);
+
         const auto save_callback = [this]() {
             const auto filename = QFileDialog::getSaveFileName(this, tr("Save Image"), "render.png",
                                                                tr("Images (*.png);;All Files (*)"));
-            QFile file(filename);
-            file.open(QIODevice::WriteOnly);
-            viewer_->getImage().save(&file, "PNG");
+            if (filename == nullptr || filename.isEmpty()) {
+                std::cerr << "No file selected." << std::endl;
+            } else {
+                QFile file(filename);
+                file.open(QIODevice::WriteOnly);
+                viewer_->getImage().save(&file, "PNG");
+            }
         };
 
-        connect(save_button_, &QPushButton::clicked, save_callback);
+        saveAction_ = new QAction(tr("&Save as ..."), this);
+        saveAction_->setShortcut(tr("Ctrl+S"));
+        saveAction_->setStatusTip(tr("Save the file with a new name."));
+        connect(saveAction_, &QAction::triggered, this, save_callback);
+
+        fileMenu_ = menuBar()->addMenu(tr("&File"));
+        fileMenu_->addAction(saveAction_);
+
+//        connect(save_button_, &QPushButton::clicked, save_callback);
 
         this->resize(width, height);
     }
 
-    ~Gui() { viewer_->stopRaytrace(); }
+    ~Gui() override { viewer_->stopRaytrace(); }
 };
